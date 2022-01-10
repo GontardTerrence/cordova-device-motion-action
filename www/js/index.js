e76225e3bd8b87be8e58e08d638f8aba009eacba
GontardTerrence <85871931+GontardTerrence@users.noreply.github.com>
2022-01-10 11:47:22 +0100

    Changes to the position traking

--- www/js/index.js
+++ www/js/index.js
@@ -25,6 +25,7 @@ function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    
     document.getElementById('deviceready').classList.add('ready');
 }
 var watch = 0;
